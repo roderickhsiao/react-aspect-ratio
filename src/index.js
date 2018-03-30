9d0c1d147e48a418f85902b85f2994be163f3f9a
Roderick Hsiao <roderick.hsiao@gotinder.com>
2018-03-29 17:51:26 -0700

    Support React 16.3

--- src/index.js
+++ src/index.js
@@ -2,20 +2,26 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
+import polyfill from 'react-lifecycles-compat';
+
 const CUSTOM_PROPERTY_NAME = '--aspect-ratio';
 
-class AspectRatio extends PureComponent {
+type Props = {
+  ratio: string,
+  children: Object
+};
+
+class AspectRatio extends PureComponent<Props> {
   node: {
     style: Object
   };
 
-  constructor(props: Object) {
-    super(props);
-    this.node;
+  state: {
+    ratio: string
+  }
 
-    this.state = {
-      ratio: props.ratio
-    };
+  state = {
+    ratio: this.props.ratio
   }
 
   static defaultProps = {
@@ -28,42 +34,31 @@ class AspectRatio extends PureComponent {
     children: PropTypes.node
   };
 
-  componentDidMount() {
-    this.updateAspectRatio('', this.props.ratio);
-  }
-
-  componentWillReceiveProps(nextProps: Object) {
-    this.updateAspectRatio(this.props.ratio, nextProps.ratio);
-  }
-
-  updateAspectRatio = (ratio: string, nextRatio: string) => {
-    if (ratio !== nextRatio) {
-      this.setState(
-        {
-          ratio: nextRatio
-        },
-        () => {
-          if (this.node) {
-            // BC for older version of React https://github.com/facebook/react/issues/6411
-            // check if React support custom property AFTER
-            const customPropertyValue = this.node.style.getPropertyValue(
-              CUSTOM_PROPERTY_NAME
-            );
-            if (!customPropertyValue) {
-              this.node.style.setProperty('--aspect-ratio', nextRatio);
-            }
-          }
-        }
-      );
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.ratio === prevState.ratio) {
+      return null;
     }
-  };
+
+    return this.getAspectRatio(nextProps.ratio);
+  }
+
+  getAspectRatio = (ratio: string) => ratio;
+
+  componentDidUpdate() {
+    if (this.node) {
+      // BC for older version of React https://github.com/facebook/react/issues/6411
+      // check if React support custom property AFTER
+      const customPropertyValue = this.node.style.getPropertyValue(CUSTOM_PROPERTY_NAME);
+      if (!customPropertyValue) {
+        this.node.style.setProperty('--aspect-ratio', nextRatio);
+      }
+    }
+  }
 
   render() {
     const { ratio, style, ...others } = this.props; // eslint-disable-line no-unused-vars
-    const newStyle = Object.assign(
-      { [CUSTOM_PROPERTY_NAME]: this.state.ratio },
-      style
-    );
+
+    const newStyle = Object.assign({ [CUSTOM_PROPERTY_NAME]: this.state.ratio }, style);
 
     return (
       <div
@@ -79,4 +74,4 @@ class AspectRatio extends PureComponent {
   }
 }
 
-export default AspectRatio;
+export default polyfill(AspectRatio);
