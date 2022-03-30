d4f428c5722a3626561ce2213d87ba4511b1efb5
Roderick Hsiao <roderickhsiao@gmail.com>
2022-03-29 23:39:04 -0700

    Move type definition step into user defined script

--- .scripts/user/prepublish.sh
+++ .scripts/user/prepublish.sh
@@ -1,1 +1,8 @@
 # Use this file to your own code to run at NPM `prepublish` event.
+
+echo ""
+echo "=> Creating type definitions into dist/types ..."
+
+yarn tsc --build tsconfig.publish.json
+
+echo "=> Type definitions created."
