fcd5aa6eb3b191f4239ce364a7d5e96b11aa5d6a
Roderick Hsiao <roderickhsiao@gmail.com>
2024-12-13 18:46:08 -0800

    fix: Update storybook to v8

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,10 @@
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-docs'],
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-webpack5-compiler-babel',
+    '@chromatic-com/storybook'
+  ],
   framework: {
     name: '@storybook/react-webpack5',
     options: {}
